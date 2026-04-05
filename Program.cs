066263ed7c7722f33a4ebc5cb27e0b78ecb797e5
Afriza Gilleon Ginting <41586762+afrizagilleon@users.noreply.github.com>
2026-04-06 05:38:27 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022300154;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("Bohemian Rhapsody");
+        track.IncreasePlayCount(500);
+        track.PrintTrackDetails();
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022300154
+{
+    internal class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random rnd = new Random();
+            this.id = rnd.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID     : {id}");
+            Console.WriteLine($"Title  : {title}");
+            Console.WriteLine($"Plays  : {playCount}");
+            Console.WriteLine("-----------------------------");
+        }
+    }
+
+}
