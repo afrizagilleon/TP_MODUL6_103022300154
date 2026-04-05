33139dd06a227b1948636d12c7067c38249d6beb
Afriza Gilleon Ginting <41586762+afrizagilleon@users.noreply.github.com>
2026-04-06 05:41:32 +0700

    menambahkan implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using TP_MODUL6_103022300154;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("=== UJI NORMAL ===");
         SayaMusicTrack track = new SayaMusicTrack("Bohemian Rhapsody");
         track.IncreasePlayCount(500);
         track.PrintTrackDetails();
+
+        //Console.WriteLine("=== UJI OVERFLOW ===");
+        //SayaMusicTrack track2 = new SayaMusicTrack("Stairway to Heaven");
+        //for (int i = 0; i < 300; i++)
+        //{
+        //    track2.IncreasePlayCount(10_000_000);
+        //}
+        //track2.PrintTrackDetails();
+
+        Console.WriteLine("=== UJI PRECONDITION NULL ===");
+         SayaMusicTrack trackNull = new SayaMusicTrack(null);
     }
 }
--- SayaMusicTrack.cs
+++ SayaMusicTrack.cs
@@ -1,35 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
 
-namespace TP_MODUL6_103022300154
+public class SayaMusicTrack
 {
-    internal class SayaMusicTrack
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
     {
-        private int id;
-        private int playCount;
-        private string title;
+        Debug.Assert(title != null, "Title tidak boleh null!");
+        Debug.Assert(title.Length <= 100, "Title maksimal 100 karakter!");
 
-        public SayaMusicTrack(string title)
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        Debug.Assert(count <= 10_000_000, "Penambahan play count maksimal 10.000.000 per panggilan!");
+
+        try
         {
-            Random rnd = new Random();
-            this.id = rnd.Next(10000, 99999);
-            this.title = title;
-            this.playCount = 0;
+            checked
+            {
+                playCount += count;
+            }
         }
-
-        public void IncreasePlayCount(int count)
+        catch (OverflowException ex)
         {
-            playCount += count;
-        }
-
-        public void PrintTrackDetails()
-        {
-            Console.WriteLine($"ID     : {id}");
-            Console.WriteLine($"Title  : {title}");
-            Console.WriteLine($"Plays  : {playCount}");
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"[ERROR] Overflow terjadi: {ex.Message}");
         }
     }
 
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine($"ID     : {id}");
+        Console.WriteLine($"Title  : {title}");
+        Console.WriteLine($"Plays  : {playCount}");
+        Console.WriteLine("-----------------------------");
+    }
 }
